91c864dcb93e652d4ce983a7855fbcb5fdef2968
unknown <rizaldymongi1@gmail.com>
2023-06-07 04:39:13 -0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      container: {
+        center: true ,
+        padding: '16px' ,
+      },
+      colors: {
+        primary: '#14b8a6' ,
+        dark: '#0f172a' ,
+      },
+      screens: {
+        '2xl': '1320px' ,
+      },
+    },
+  },
+  plugins: [],
+}
+
